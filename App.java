ba4153fcf9e05e221a1de5ffe0796bd034b7a28b
Byron <byronblocker@gmail.com>
2017-05-31 15:15:35 -0700

    done fer the day

--- App.java
+++ App.java
@@ -16,13 +16,27 @@ public class App {
     Choice choice4 = new Choice();
     Choice choice5 = new Choice();
 
+    System.out.println("+===========================+");
+    System.out.println("+---------------------------+");
+    System.out.println("+===Decision  Indecisions===+");
+    System.out.println("+-----------by--------------+");
+    System.out.println("+=======Byron=Blocker=======+");
+    System.out.println("+---------------------------+");
+    System.out.println("+===========================+");
+    System.out.println();
+    System.out.println("I'm here to help indecisive people make choices");
+    System.out.println("but first...");
+    System.out.println();
+
+    System.out.println("What is your name?");
+    player1.setName(console.readLine());
+    System.out.println("And what is your friend's name?");
+    player2.setName(console.readLine());
+    System.out.println();
+    System.out.println("Super awesome, well, " + player1.name + " and " + player2.name);
+
     while (appRunning){
-      System.out.println("What is player 1's name?");
-      player1.setName(console.readLine());
-      System.out.println("What is player 2's name?");
-      player2.setName(console.readLine());
-
-
+      System.out.println("LET'S MAKE SOME CHOICES!!!");
       player1.playerTurn();
       choice1.setName(console.readLine());
       player2.playerTurn();
@@ -32,7 +46,8 @@ public class App {
       player2.playerTurn();
       choice4.setName(console.readLine());
 
-      System.out.println("And whose idea was it to use the app?");
+      System.out.println();
+      System.out.println("And whose idea was it to use this awesome app?");
       System.out.println("...cuz they totes get an extra turn..");
       String wildCard = console.readLine();
       if (wildCard.equals(player1.name)){
@@ -46,13 +61,37 @@ public class App {
 
       Choice result = decisionTime.decision(choice1, choice2, choice3, choice4, choice5);
 
-
-      System.out.println("Choice Made!");
-      System.out.println(result.name);
-
+      System.out.println();
+      System.out.println("Good choices... let me think about it");
+      System.out.println();
+      System.out.println("##");
+      System.out.println("####");
+      System.out.println("######");
+      System.out.println("########");
+      System.out.println("###########");
+      System.out.println("#Choice Made#");
+      System.out.println();
+      System.out.println("*******it is time for*******");
+      System.out.println();
+      System.out.println(result.name.toUpperCase() +  " " + result.name.toUpperCase() + " " + result.name.toUpperCase());
+      System.out.println();
+      System.out.println("That wasn't so hard, was it?");
+      System.out.println();
+      System.out.println("          8====D   O        ");
+      System.out.println();
+      System.out.println();
       System.out.println("Would you like to play again or quit?");
       String appNav = console.readLine();
       if (appNav.equals("quit")){
+
+        System.out.println("+===========================+");
+        System.out.println("+---------------------------+");
+        System.out.println("+===========Goodbye=========+");
+        System.out.println("+---------------------------+");
+        System.out.println("+============Human==========+");
+        System.out.println("+---------------------------+");
+        System.out.println("+===========================+");
+
         appRunning = false;
       }
     }
