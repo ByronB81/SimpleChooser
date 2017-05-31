7ab61dec2b83b945398fa73edc661d57fab85d44
Byron <byronblocker@gmail.com>
2017-05-31 13:28:11 -0700

    got game working with full logic

--- Chooser.java
+++ Chooser.java
@@ -7,16 +7,29 @@ public class Chooser {
 
     Choice choice1 = new Choice();
     Choice choice2 = new Choice();
+    Choice choice3 = new Choice();
+    Choice choice4 = new Choice();
+    Choice choice5 = new Choice();
 
     System.out.println("Please name your first choice");
     String choice1Input = console.readLine();
     System.out.println("Please name your second choice");
     String choice2Input = console.readLine();
+    System.out.println("Please name your third choice");
+    String choice3Input = console.readLine();
+    System.out.println("Please name your fourth choice");
+    String choice4Input = console.readLine();
+    System.out.println("Please name your fifth choice");
+    String choice5Input = console.readLine();
 
     choice1.name = choice1Input;
     choice2.name = choice2Input;
+    choice3.name = choice3Input;
+    choice4.name = choice4Input;
+    choice5.name = choice5Input;
 
-    Choice result = decisionTime.decision(choice1, choice2);
+
+    Choice result = decisionTime.decision(choice1, choice2, choice3, choice4, choice5);
 
 
     System.out.println("Choice Made!");
--- Game.java
+++ Game.java
@@ -2,10 +2,16 @@ import java.util.Random;
 
 public class Game {
 
-  public static Choice decision(Choice choice1, Choice choice2) {
+  public static Choice decision(Choice choice1, Choice choice2, Choice choice3, Choice choice4, Choice choice5) {
     Random rand = new Random();
-    int rnJesus = rand.nextInt(2) + 1;
-    if (rnJesus == 2) {
+    int rnJesus = rand.nextInt(5) + 1;
+    if (rnJesus == 5) {
+      return choice5;
+    } else if (rnJesus == 4) {
+      return choice4;
+    } else if (rnJesus == 3) {
+      return choice3;
+    } else if (rnJesus == 2) {
       return choice2;
     } else {
       return choice1;
