8d89a6435f640e73b279b3d54140058ac22d1ad8
Byron <byronblocker@gmail.com>
2017-05-31 13:41:08 -0700

    finish up player object

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player {
+  String name;
+  Choice playerChoice1;
+  Choice playerChoice2;
+
+  public void setName(String nameSetter){
+    this.name = nameSetter;
+  }
+
+  public void setChoice1(Choice choiceSetter) {
+    this.playerChoice1 = choiceSetter;
+  }
+
+  public void setChoice2(Choice choiceSetter) {
+    this.playerChoice2 = choiceSetter;
+  }
+
+}
